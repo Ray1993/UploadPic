71e329501ec56c5f943e33ad87ed58571f6e8173
Ray1993 <597176448@qq.com>
2017-08-17 16:08:21 +0800

    2.0 init

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,12 @@
-<template>
+ <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <div class="pic-list">
+        <ul>
+            <li><a href="javascript:;"><img :src="addicon" alt="选择图片"></a></li>
+        </ul>
+    </div>
+    <input type="button" name="upload" value="确认上传" class="submit">
+    <input type="file" hidden id="picfile">
   </div>
 </template>
 
@@ -24,7 +15,8 @@ export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Your Vue.js App',
+      addicon:'src/assets/add.png'
     }
   }
 }
@@ -36,25 +28,52 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #3D3D3D;
+  max-width: 1112px;
+  margin:60px auto; 
 }
-
-h1, h2 {
-  font-weight: normal;
+ul{
+  list-style: none;
+  overflow: hidden
 }
-
-ul {
-  list-style-type: none;
-  padding: 0;
+.pic-list{
+  max-width: 1112px;
+  margin: 0px auto 50px auto;
+  border: 2px solid #3D3D3D;
+  border-radius: 5px;
+  padding: 50px 10px;
 }
-
-li {
-  display: inline-block;
-  margin: 0 10px;
+.pic-list ul li{
+   float: left;
+   width: 20%;
+   height: 200px;
+   margin:0 2.5%;
+   position: relative;
 }
-
-a {
-  color: #42b983;
+.pic-list ul li a{
+   width: 100%;
+   height: 100%;
+   position: relative;
+   display: block
+}
+.pic-list ul li img{
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  left: 0px;
+  top: 0px;
+}
+.submit{
+  width: 100px;height: 50px;
+  background-color: transparent;
+  border: 2px solid #3D3D3D;
+  float: right;
+  color:#3D3D3D ;
+  font-size: 16px;
+  border-radius: 5px;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-weight: bold;
+  cursor: pointer;
 }
 </style>
