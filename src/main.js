4d0d084233c9a1ddf8a10b76136357427130acf2
Ray1993 <597176448@qq.com>
2017-09-08 18:07:03 +0800

    1.01 配置vue-resource vue-router

--- src/main.js
+++ src/main.js
@@ -1,19 +1,28 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import VueResource from 'vue-resource';
 import App from './App';
-import goods from './components/goods/goods';
+import goods from 'components/goods/goods';
+import ratings from 'components/ratings/ratings';
+import seller from 'components/seller/seller';
+
+import 'common/stylus/index.styl';
 
 Vue.use(VueRouter);
+Vue.use(VueResource);
 
 const routes = [
-  {path: '/goods', component: goods}
-  // {path: '/ratings', component: goods},
-  // {path: '/goods', component: goods}
+  {path: '/goods', component: goods},
+  {path: '/ratings', component: ratings},
+  {path: '/seller', component: seller}
 ];
 
 const router = new VueRouter({
-  routes
+  routes,
+  linkActiveClass: 'active'
 });
+
+router.push('/goods');
 
 /* eslint-disable no-new */
 new Vue({
@@ -21,3 +30,4 @@ new Vue({
   router,
   render: h => h(App)
 });
+
